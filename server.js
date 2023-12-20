0c5fbf7cd9fced6c39d817cc455da83c2942a305
PFranco1 <132111535+PFranco1@users.noreply.github.com>
2023-12-19 19:51:49 -0600

    Front page updates and contact page
    Massive push to main large changes should be noted as front page, contact page creation, removal of top icon and many other cosmetic changes. TO DO: mobile first view needs to be maintained for the final version.

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const primaryNav = document.querySelector('.primary-navigation');
 const navToggle = document.querySelector('.mobile-nav-toggle');
 
+
 navToggle.addEventListener('click', () => {
     const visibility = primaryNav.getAttribute('data-visible');
   
@@ -15,4 +16,24 @@ navToggle.addEventListener('click', () => {
     navToggle.classList.toggle('active');
 });
 
+//Create 2 new email addresses to send and recive emails
 
+function sendEmail(){
+  var email = document.getElementById("name").value;
+  var phone = document.getElementById("phone").value;
+  var name = document.getElementById("email").value;
+  var subject = document.getElementById("subject").value;
+
+  var completeSubject = "Email: " + email + " | Phone: " + phone + " | Name: " + name + " | " + subject;
+
+  Email.send({
+    SecureToken: "f011916c-36f5-41bb-8c12-951d8fb8c308",
+    Port: "2525",
+    To : 'pfranco5@utexas.edu',
+    From : 'spamemail2093411@gmail.com',
+    Subject : completeSubject,
+    Body : document.getElementById("body").value
+}).then(
+  message => alert(message)
+);
+}
