c000adbe4ce8e11371e2e8fef8ea2a1d8a8d95b6
PFranco1 <132111535+PFranco1@users.noreply.github.com>
2024-02-17 22:35:34 -0600

    node js transfer

--- server_tracked.js
+++ server_tracked.js
@@ -18,6 +18,17 @@ fetch("https://ipapi.co/json/")
   console.log(responseJson);
 });
 
+const winston = require('winston');
+
+const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.json(),
+  transports: [
+    new winston.transports.File(new winston.transports.Console())
+  ]
+});
+
+logger.info('This is a log message')
   
 
 
